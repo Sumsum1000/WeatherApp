9ca2cebf44c223cf55354586cf230018e362ee98
Asaf Levi <asafl14@yahoo.com>
2023-01-07 16:51:21 +0200

    favorites styling added, bar sticky position added

--- src/Components/Favorites/favoriteCity.js
+++ src/Components/Favorites/favoriteCity.js
@@ -1,45 +1,60 @@
-import style from './favorites.module.scss';
-import { fetchCityStart } from '../fetchActions';
-import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { favoritesListActions, weatherDataActions } from '../Store/store';
-import { FavoriteDay } from './favoriteDay';
+import style from "./favorites.module.scss";
+import { fetchCityStart } from "../fetchActions";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { favoritesListActions, weatherDataActions } from "../Store/store";
+import { FavoriteDay } from "./favoriteDay";
 
 export const FavoriteCity = ({
-    id, name, src1, src2, src3, src4, temp1, temp2, temp3, temp4, onClick}) => {
+  id,
+  name,
+  src1,
+  src2,
+  src3,
+  src4,
+  temp1,
+  temp2,
+  temp3,
+  temp4,
+  onClick,
+}) => {
+  const dispatch = useDispatch();
+  const list = useSelector((state) => state.favoritesList.cities);
+  const [dataToMap, setDataToMap] = useState([
+    { dt_txt: "", weather: [{ icon: "" }], temp0: "" },
+  ]);
+  let finalData;
 
-    const dispatch = useDispatch();
-    const list = useSelector((state) => state.favoritesList.cities)
-    const [dataToMap, setDataToMap] = useState([
-        {dt_txt: '', weather: [{icon: ''}], temp0: ''}
-    ]);
-    let finalData;
+  const fetchCity = async (name) => {
+    const cityStart = await fetchCityStart("Tokyo");
+    const newLat = await cityStart[0].lat;
+    const newLon = await cityStart[0].lon;
+    const cityUrl = `
+        https://api.openweathermap.org/data/2.5/forecast?lat=${newLat}&lon=${newLon}&appid=ab96154ce8f20e07812ea1417c9a0c0c`;
+    const cityDetails = await fetch(cityUrl);
+    const cityDetailsJson = await cityDetails.json();
+    finalData = await cityDetailsJson.list.filter(
+      (element) => element.dt_txt.split("").splice(11).join("") === "12:00:00"
+    );
 
-    const fetchCity = async(name) => {
-        const cityStart = await fetchCityStart('Tokyo');
-        const newLat = await cityStart[0].lat;
-        const newLon = await cityStart[0].lon;
-        const cityUrl =`
-        https://api.openweathermap.org/data/2.5/forecast?lat=${newLat}&lon=${newLon}&appid=ab96154ce8f20e07812ea1417c9a0c0c` 
-        const cityDetails = await fetch(cityUrl);
-        const cityDetailsJson = await cityDetails.json();
-        finalData = await cityDetailsJson.list.filter(element => element.dt_txt.split('').splice(11).join('') === "12:00:00")
+    setDataToMap(finalData);
+  };
 
-        setDataToMap(finalData);
-    }
+  useEffect(() => {
+    fetchCity();
+  }, []);
 
-    useEffect(() => {
-        fetchCity();
-    }, [])
+  return (
+    <div className={[style["favorite-city"]]} id={id}>
+      <div className={[style["city-name"]]}>
+        <button onClick={onClick}>x</button>
+        <h3>{name}</h3>
+      </div>
 
-    return(
-        <div className={[style['favorite-city']]} id={id}>
-            <h3>{name}</h3>
-            <FavoriteDay src={src1} temp={temp1} />
-            <FavoriteDay src={src2} temp={temp2} />
-            <FavoriteDay src={src3} temp={temp3} />
-            <FavoriteDay src={src4} temp={temp4} />
-            <button onClick={onClick}>x</button>
-        </div> 
-    )
-}
+      <FavoriteDay src={src1} temp={temp1} />
+      <FavoriteDay src={src2} temp={temp2} />
+      <FavoriteDay src={src3} temp={temp3} />
+      <FavoriteDay src={src4} temp={temp4} />
+    </div>
+  );
+};
--- src/Components/content.js
+++ src/Components/content.js
@@ -1,140 +1,160 @@
-import style from './content.module.scss';
-import placeholder from '../placeholder.jpg';
-import favorite from '../Favorite_icon.png';
+import style from "./content.module.scss";
+import placeholder from "../placeholder.jpg";
+import favorite from "../Favorite_icon.png";
 
-import { IconCard } from './IconCard';
+import { IconCard } from "./IconCard";
 //-----------------------------------------------
-import { useState, useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { currentCityActions, testActions } from "./Store/store";
-import { onStartActiuons } from './Store/store';
-import { favoritesListActions, favoritesDataActions } from '.././Components/Store/store';
-import { fetchCity, fetchCityData, extractHours, finalCityData } from './currentCity'
-
+import { onStartActiuons } from "./Store/store";
+import {
+  favoritesListActions,
+  favoritesDataActions,
+} from ".././Components/Store/store";
+import {
+  fetchCity,
+  fetchCityData,
+  extractHours,
+  finalCityData,
+} from "./currentCity";
 
 export const Content = () => {
+  localStorage.setItem("isFirstTime", 0);
+  const inputRef = useRef();
+  const [listData, setListData] = useState([]);
 
-        localStorage.setItem('isFirstTime', 0);
-        const inputRef = useRef();
-        const [listData, setListData] = useState([]);
+  const dispatch = useDispatch();
 
-        const dispatch = useDispatch();
+  const fetchDays = async (cityName) => {
+    const cityData = await fetchCity(cityName);
+    const allData = await fetchCityData(cityData);
 
-        const fetchDays = async(cityName) => {
-        const cityData = await fetchCity(cityName)
-        const allData = await fetchCityData(cityData) 
-        
-        const extractData = await extractHours(allData)
-        const finalData = finalCityData(extractData, cityName);
-        const id = Math.random();
-        finalData.id = id;
-        finalData.heartColor = 'red';
-        setListData([...listData, finalData])
-        return finalData
-        }
+    const extractData = await extractHours(allData);
+    const finalData = finalCityData(extractData, cityName);
+    const id = Math.random();
+    finalData.id = id;
+    finalData.heartColor = "red";
+    setListData([...listData, finalData]);
+    return finalData;
+  };
 
-    const currentCity = useSelector(state => state.currentCity); 
-    const firstTime = useSelector(state => state.isFirstTime);
-    const favorites = useSelector(state => state.favoritesList.cities);
-    const data = useSelector((state) => state.cityData);
-    //const heartColor = useSelector(state => state.heartColor)
-    
-    const fetchCityHandler = (e) => {
-        if (e.keyCode === 13) {
-            if (inputRef.current.value === '' || inputRef.current.value === undefined) {
-                console.log('Please enter a valid name');
-            }
-            else {
-                 const lowerCaseName = inputRef.current.value.toLowerCase();
-                 fetchDays(lowerCaseName)
-                .then(data => dispatch(currentCityActions.setCurrentCity(data)))
-            }
-        }
+  const currentCity = useSelector((state) => state.currentCity);
+  const firstTime = useSelector((state) => state.isFirstTime);
+  const favorites = useSelector((state) => state.favoritesList.cities);
+  const data = useSelector((state) => state.cityData);
+  //const heartColor = useSelector(state => state.heartColor)
+
+  const fetchCityHandler = (e) => {
+    if (e.keyCode === 13) {
+      if (
+        inputRef.current.value === "" ||
+        inputRef.current.value === undefined
+      ) {
+        console.log("Please enter a valid name");
+      } else {
+        const lowerCaseName = inputRef.current.value.toLowerCase();
+        fetchDays(lowerCaseName).then((data) =>
+          dispatch(currentCityActions.setCurrentCity(data))
+        );
+      }
     }
+  };
 
-
-    const favoriteHandler = (city) => {
-        const isCity = favorites.includes(city.toLowerCase())
-        if (!isCity) {
-            dispatch(favoritesListActions.addToFavorite(city))
-            fetchDays(currentCity.name)
-            .then(data => dispatch(favoritesDataActions.addToFavoritesData(data))) 
-        }
-            
+  const favoriteHandler = (city) => {
+    const isCity = favorites.includes(city.toLowerCase());
+    if (!isCity) {
+      dispatch(favoritesListActions.addToFavorite(city));
+      fetchDays(currentCity.name).then((data) =>
+        dispatch(favoritesDataActions.addToFavoritesData(data))
+      );
     }
+  };
 
-
-    useEffect(() => {
+  useEffect(() => {
     // Load Tel Aviv only first time
-       if (firstTime === false) {
-            fetchDays('tel-aviv')
-            .then(data => dispatch(currentCityActions.setCurrentCity(data)))
-            dispatch(onStartActiuons.setIsFirstTime());
-       }
+    if (firstTime === false) {
+      fetchDays("tel-aviv").then((data) =>
+        dispatch(currentCityActions.setCurrentCity(data))
+      );
+      dispatch(onStartActiuons.setIsFirstTime());
+    }
 
-       const isInFaforites = favorites.includes(currentCity.name);
-       dispatch(currentCityActions.toggleHeartColor(isInFaforites))
-    }, [])
+    const isInFaforites = favorites.includes(currentCity.name);
+    dispatch(currentCityActions.toggleHeartColor(isInFaforites));
+  }, []);
 
-    useEffect(() => {
-        const isInFaforites = favorites.includes(currentCity.name);
-        dispatch(currentCityActions.toggleHeartColor(isInFaforites))
-    }, [favorites])
+  useEffect(() => {
+    const isInFaforites = favorites.includes(currentCity.name);
+    dispatch(currentCityActions.toggleHeartColor(isInFaforites));
+  }, [favorites]);
 
-    useEffect(() => {
-        console.log('CurrentCity ', currentCity);
-        const isInFaforites = favorites.includes(currentCity.name);
-        dispatch(currentCityActions.toggleHeartColor(isInFaforites))
-    }, [currentCity])
+  useEffect(() => {
+    console.log("CurrentCity ", currentCity);
+    const isInFaforites = favorites.includes(currentCity.name);
+    dispatch(currentCityActions.toggleHeartColor(isInFaforites));
+  }, [currentCity]);
 
-    return(
-        <div className={[style['content']]}>
-            <div className={style['favorites-container']}>
-                <input 
-                    className={style['input-city']} 
-                    ref={inputRef} 
-                    placeholder='Enter a city'
-                    onKeyDown={(e) => fetchCityHandler(e)}
-                />
-                <svg xmlns="http://www.w3.org/2000/svg" 
-                  onClick={() => favoriteHandler(currentCity.name)}
-                  width="28" height="28" viewBox="0 0 24 24"><path fillOpacity={1} fill={currentCity.heartColor} d="M12 9.229c.234-1.12 1.547-6.229 5.382-6.229 2.22 0 4.618 1.551 4.618 5.003 0 3.907-3.627 8.47-10 12.629-6.373-4.159-10-8.722-10-12.629 0-3.484 2.369-5.005 4.577-5.005 3.923 0 5.145 5.126 5.423 6.231zm-12-1.226c0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-7.962-9.648-9.028-12-3.737-2.338-5.262-12-4.27-12 3.737z"/></svg>
-            </div>
-            {/* <h1>{list}</h1> */}
-             {/* Current weather */}
-                <div className={style['grid-current']}>
-                    <img src={`http://openweathermap.org/img/wn/${currentCity.days[0].icon}@4x.png`} className={style['test']}/>
-                    <div className={style['deg-container']}>
-                        <h3 className={style['namecurrent']}>{currentCity.name}</h3>
-                        <p className={style['deg-current']}>
-                                 {currentCity.days[0].temp}
-                        </p>
-                    </div>
-                    <p className={[style['descreption']]}>{currentCity.days[0].description}</p>
-                </div>
-                <div className={style['five-days-container']}>
-                    <IconCard  
-                        src={`http://openweathermap.org/img/wn/${currentCity.days[1].icon}@4x.png`}
-                        date={currentCity.days[1].date}
-                        temp={currentCity.days[1].temp}
-                    />
-                    <IconCard 
-                        src={`http://openweathermap.org/img/wn/${currentCity.days[2].icon}@4x.png`}
-                        date={currentCity.days[2].date}
-                        temp={currentCity.days[2].temp}
-                    />
-                    <IconCard 
-                        src={`http://openweathermap.org/img/wn/${currentCity.days[3].icon}@4x.png`}
-                        date={currentCity.days[3].date}
-                        temp={currentCity.days[3].temp}
-                    />
-                    <IconCard 
-                        src={`http://openweathermap.org/img/wn/${currentCity.days[4].icon}@4x.png`}
-                        date={currentCity.days[4].date}
-                        temp={currentCity.days[4].temp}
-                    />
-                </div>
+  return (
+    <div className={[style["content"]]}>
+      <div className={style["favorites-container"]}>
+        <input
+          className={style["input-city"]}
+          ref={inputRef}
+          placeholder="Enter a city"
+          onKeyDown={(e) => fetchCityHandler(e)}
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          onClick={() => favoriteHandler(currentCity.name)}
+          width="28"
+          height="28"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fillOpacity={1}
+            fill={currentCity.heartColor}
+            d="M12 9.229c.234-1.12 1.547-6.229 5.382-6.229 2.22 0 4.618 1.551 4.618 5.003 0 3.907-3.627 8.47-10 12.629-6.373-4.159-10-8.722-10-12.629 0-3.484 2.369-5.005 4.577-5.005 3.923 0 5.145 5.126 5.423 6.231zm-12-1.226c0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-7.962-9.648-9.028-12-3.737-2.338-5.262-12-4.27-12 3.737z"
+          />
+        </svg>
+      </div>
+      {/* <h1>{list}</h1> */}
+      {/* Current weather */}
+      <div className={style["grid-current"]}>
+        <img
+          src={`http://openweathermap.org/img/wn/${currentCity.days[0].icon}@4x.png`}
+          className={style["test"]}
+        />
+        <div className={style["deg-container"]}>
+          <h3 className={style["namecurrent"]}>{currentCity.name}</h3>
+          <p className={style["deg-current"]}>{currentCity.days[0].temp}</p>
         </div>
-        
-    )
-}
+        <p className={[style["descreption"]]}>
+          {currentCity.days[0].description}
+        </p>
+      </div>
+      <div className={style["five-days-container"]}>
+        <IconCard
+          src={`http://openweathermap.org/img/wn/${currentCity.days[1].icon}@4x.png`}
+          date={currentCity.days[1].date}
+          temp={currentCity.days[1].temp}
+        />
+        <IconCard
+          src={`http://openweathermap.org/img/wn/${currentCity.days[2].icon}@4x.png`}
+          date={currentCity.days[2].date}
+          temp={currentCity.days[2].temp}
+        />
+        <IconCard
+          src={`http://openweathermap.org/img/wn/${currentCity.days[3].icon}@4x.png`}
+          date={currentCity.days[3].date}
+          temp={currentCity.days[3].temp}
+        />
+        <IconCard
+          src={`http://openweathermap.org/img/wn/${currentCity.days[4].icon}@4x.png`}
+          date={currentCity.days[4].date}
+          temp={currentCity.days[4].temp}
+        />
+      </div>
+    </div>
+  );
+};
